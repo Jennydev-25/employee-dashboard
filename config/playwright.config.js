419e7dc9cbb8c496dade24620df1317127cd90cb
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-06-07 23:51:41 +0200

    test: add e2e logout test with playwright

--- config/playwright.config.js
+++ config/playwright.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from '@playwright/test';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-    testDir: './tests/e2e',
+    testDir: resolve(__dirname, '../tests/e2e'),
+    testMatch: '**/*.spec.js',
     use: {
         baseURL: 'http://127.0.0.1:5500',
     },
